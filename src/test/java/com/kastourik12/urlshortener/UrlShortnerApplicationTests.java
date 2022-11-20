e4418abc18657c26f132be324e9a20f357350ca7
kastourik12 <kastouriw@yahoo.Fr>
2022-11-20 16:24:01 +0100

    first commit

---
+++ src/test/java/com/kastourik12/urlshortener/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kastourik12.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
