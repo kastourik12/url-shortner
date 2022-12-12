c33763f5c2e5284bc1dbc032ed674de1679261ce
kastourik12 <kastouriw@yahoo.fr>
2022-12-12 11:31:13 +0100

    statistics controller: getAll test

--- src/main/java/com/kastourik12/urlshortener/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/kastourik12/urlshortener/exceptions/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ package com.kastourik12.urlshortener.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,12 +23,6 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> requestPayloadValidation(MethodArgumentNotValidException exception){
         return new ResponseEntity<>(exception.getFieldError().getDefaultMessage(),HttpStatus.BAD_REQUEST);
     }
-
-   @ExceptionHandler(value = Exception.class)
-   public ResponseEntity<?> globalExceptions(Exception exception){
-      log.error(exception.getClass().getName(),exception.getMessage());
-       return new ResponseEntity<>("Service unavailable try later ",HttpStatus.INTERNAL_SERVER_ERROR);
-   }
 
    @ExceptionHandler(value = CustomException.class)
     public ResponseEntity<?> customExceptionHandling(CustomException exception){
---
+++ src/test/java/com/kastourik12/urlshortener/controllers/StatisticsControllerTest.java
@@ -0,0 +1,63 @@
+package com.kastourik12.urlshortener.controllers;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class StatisticsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnUnauthorizedWithNoAuthForGetAll() throws Exception  {
+        mockMvc.perform(
+                get("/stats/all"))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void shouldReturnUnauthorizedWithUserAuthForGetAll() throws Exception {
+        mockMvc.perform(
+                        get("/stats/all")
+                                .with(jwt().authorities(
+                                        new SimpleGrantedAuthority("SCOPE_ROLE_USER")
+                                ))
+                )
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+    @Test
+    void shouldReturn200WithAdminAuthForGetAll() throws Exception{
+        mockMvc.perform(
+                    get("/stats/all")
+                        .with(jwt().authorities(
+                                new SimpleGrantedAuthority("SCOPE_ROLE_ADMIN")
+                        )))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getUrlVisits() {
+    }
+
+    @Test
+    void getUserUrlVisits() {
+    }
+}
